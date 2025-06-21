abf255650916cff12eb3ce0351e6bdc91a1c67f3
Dasari Mahendra <mahendradasari2003@gmail.com>
2025-06-21 15:33:17 +0530

    Update TestDriver.java

--- Project/TestDriver.java
+++ Project/TestDriver.java
@@ -5,12 +5,12 @@ import java.util.*;
 public class TestDriver {
 
 	public static void main(String[] args) {
-		ArrayList<Driver> drivers = new ArrayList<Driver>();
+	ArrayList<Driver> drivers = new ArrayList<Driver>();
 
-		 drivers.add(new Driver("Car", 101, "Sudhagar", 4200));
-	     drivers.add(new Driver("Auto", 102, "Arun", 3100));
-	     drivers.add(new Driver("Car", 103, "Venkat", 2800));
-         drivers.add(new Driver( "Lorry", 104, "Ravi", 6000));
+	 drivers.add(new Driver("Car", 101, "Sudhagar", 4200));
+	 drivers.add(new Driver("Auto", 102, "Arun", 3100));
+	 drivers.add(new Driver("Car", 103, "Venkat", 2800));
+       	 drivers.add(new Driver( "Lorry", 104, "Ravi", 6000));
          drivers.add(new Driver( "Car", 105, "Kumar", 1500));
 	
          Travel travel = new Travel();
